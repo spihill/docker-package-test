38adac36b58694eac0d0f783eab004f335edd176
spihill <spihill.in@gmail.com>
2020-02-01 12:38:11 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone https://${user_name}:${clone_token}@github.com/${user_name}/${library_repo}.git
+git clone https://${YOUR_GITHUB_USER_NAME}:${CLONE_TOKEN}@github.com/${YOUR_GITHUB_USER_NAME}/${YOUR_GITHUB_REPOSITORY}.git
 
 cd ${2} && oj-verify run --jobs 2
