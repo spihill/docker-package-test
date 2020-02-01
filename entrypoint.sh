f0825e3d5145b0f07c4b4de9f1713740fdb12bf3
spihill <spihill.in@gmail.com>
2020-02-01 12:51:53 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 # $1 username
 # $2 library_repo
 # $3 token
+# $4 CXX
 
 git clone https://${1}:${3}@github.com/${1}/${2}.git
 
-cd ${2} && oj-verify run --jobs 2
+cd ${2} && env CXX=${4} oj-verify run --jobs 2
