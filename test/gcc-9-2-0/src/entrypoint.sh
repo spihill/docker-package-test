386d91659e1af17855b6723c10821cd2da924b7c
spihill <spihill.in@gmail.com>
2020-02-02 17:22:09 +0900

    test

--- test/gcc-9-2-0/src/entrypoint.sh
+++ test/gcc-9-2-0/src/entrypoint.sh
@@ -8,4 +8,5 @@ cd /src
 git init
 g++ -dumpversion
 export CXX=${1}
-env -u GITHUB_TOKEN test.cpp -std=c++17 -Wall && ./a.out
+env -u GITHUB_TOKEN oj-verify run
+${1} test.cpp -std=c++17 -Wall && ./a.out
